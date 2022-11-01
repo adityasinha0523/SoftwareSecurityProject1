a6c478fbbfeed504293c2ad205a7fbb805f9d52f
Aditya Sinha <adityasinhafb@gmail.com>
2022-10-31 17:00:26 -0700

    Update time

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.myapplication;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private Button broadcastButton;
     private Button serviceButton;
     private Button contentButton;
+    private TextView time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +46,19 @@ public class MainActivity extends AppCompatActivity {
                 contentActivityEvent();
             }
         });
+
+        Date date = Calendar.getInstance().getTime();
+        String formatdate= date.toString();
+        String []formatdateArray= formatdate.split(" ");
+        Log.d(TAG, "myLogDate: " + date.toString());
+        Log.d(TAG, "formatdate: " + formatdate);
+        Log.d(TAG, "formatdateArray: " + formatdateArray[3]);
+
+        time= findViewById(R.id.time);
+        time.setText(formatdateArray[3]);
+
+
+
     }
     private void broadcastActivityEvent() {
         Intent intent = new Intent(this, BroadcastActivity.class);
