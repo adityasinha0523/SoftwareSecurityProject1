2ac6e76cf07c6e34f96ad5fda3c4e33eef1af03e
Preethi Shankar <pshanka8@asu.edu>
2022-11-07 17:42:10 -0700

    Update BroadcastActivity.java

--- app/src/main/java/com/example/myapplication/BroadcastActivity.java
+++ app/src/main/java/com/example/myapplication/BroadcastActivity.java
@@ -3,6 +3,14 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 
 public class BroadcastActivity extends AppCompatActivity {
 
@@ -10,5 +18,12 @@ public class BroadcastActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_broadcast);
+    }
+    
+    public void onReceive(Context context, Intent intent) {
+        String format = "HH:mm:ss";
+        SimpleDateFormat sdf =new SimpleDateFormat(format, Locale.US);
+        String callstarttime = sdf.format(Calendar.getInstance().getTime());
+        Toast.makeText(context, callstarttime, Toast.LENGTH_SHORT).show();
     }
 }
