b4058af8e283cd064789b710de0311dd46596ff6
Aditi Narendran <aditinarendran01@gmail.com>
2022-11-09 10:06:40 -0700

    ADD SERVICES

--- app/src/main/java/com/example/myapplication/ServiceActivity.java
+++ app/src/main/java/com/example/myapplication/ServiceActivity.java
@@ -3,12 +3,48 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.os.IBinder;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ComponentName;
+import android.content.ServiceConnection;
+import com.example.myapplication.BoundService.MyLocalBinder;
+import android.view.View;
+import android.widget.TextView;
 
 public class ServiceActivity extends AppCompatActivity {
+
+    BoundService myService;
+    boolean isBound = false;
+
+    public void showTime(View view)
+    {
+        String currentTime = myService.getCurrentTime();
+        TextView myTextView = (TextView)findViewById(R.id.myTextView);
+        myTextView.setText(currentTime);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+        Intent intent = new Intent(this, BoundService.class);
+        bindService(intent, myConnection, Context.BIND_AUTO_CREATE);
     }
+
+    private ServiceConnection myConnection = new ServiceConnection()
+    {
+
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            MyLocalBinder binder = (MyLocalBinder) service;
+            myService = binder.getService();
+            isBound = true;
+        }
+
+        public void onServiceDisconnected(ComponentName arg0) {
+            isBound = false;
+        }
+    };
 }
